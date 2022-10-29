c0c3e524f65ef6def66d9b47a30c3eb415ba5925
Ladii1 <faroukawe201@gmail.com>
2022-10-29 03:51:54 +0100

    week5

--- musicapp/models.py
+++ musicapp/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+class Artiste(models.Model):
+    first_name = models.TextField()
+    last_name = models.TextField()
+    age = models.IntegerField()
+
+    def __str__(self):
+        return (self.first_name)
+
+class Song(models.Model):
+    title= models.TextField()
+    artist= models.TextField()
+    image= models.ImageField()
+    date_released = models.DateField(default=datetime.today)
+    likes = models.IntegerField()
+    artiste_Id = models.IntegerField()
+
+    def __str__(self):
+        return self.title
+    
+class Lyric(models.Model):
+    song = models.ForeignKey()
+    content = models.TextField()
+    song_Id = models.IntegerField()
+    
+    def __str__(self):
+        return(self.song)
